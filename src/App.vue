3c7414aeaa27c51abb0911270435eebebd43d1ea
simone-danzeri <s.danzeri@icloud.com>
2024-04-15 14:43:37 +0200

    feat: header

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
 <script>
+import axios from 'axios';
+import { store } from './store.js';
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  components: {
+    AppHeader
+  }
+}
 
 </script>
 
 <template>
+<AppHeader></AppHeader>
 
+<main>
+  
+</main>
 </template>
 
-<style>
-
+<style lang="scss">
+@use './style/generic';
 </style>
