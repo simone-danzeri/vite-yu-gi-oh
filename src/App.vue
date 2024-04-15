22dc8444dcd4d142fd20ac945fb0e1824cac79ba
simone-danzeri <s.danzeri@icloud.com>
2024-04-15 15:16:48 +0200

    feat: popolato l'arrat dentro lo store con le info sulle card

--- src/App.vue
+++ src/App.vue
@@ -8,19 +8,41 @@ export default {
   components: {
     AppHeader,
     AppCardList
+  },
+  data() {
+    return {
+      store,
+      myParams: {
+        num: 20,
+        offset: 0
+      }
+    };
+  },
+  methods: {
+    getYugiohInfoFromApi() {
+      axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php', {
+        params: this.myParams
+      })
+      .then((response) => {
+        store.yugioh = response.data.data;
+        console.log(store.yugioh);
+      });
+    }
+  },
+  mounted() {
+    this.getYugiohInfoFromApi();
   }
 }
-
 </script>
 
 <template>
-<AppHeader></AppHeader>
+  <AppHeader></AppHeader>
 
-<main>
-<AppCardList></AppCardList>
-</main>
+  <main>
+    <AppCardList></AppCardList>
+  </main>
 </template>
 
 <style lang="scss">
-@use './style/generic';
+@use "./style/generic";
 </style>
